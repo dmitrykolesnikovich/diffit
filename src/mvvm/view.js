e63b7d11fd5b8ca904c6039beee316fc40fae6d7
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2023-07-15 16:10:18 +0300

    resizeApplication

--- src/mvvm/view.js
+++ src/mvvm/view.js
@@ -86,37 +86,3 @@ function initializeView(view, level) {
     view.pivot.y = view.height / 2;
     return {failures, layerA, layerB, successA, successB, scoreLabel, mistakesLabel};
 }
-
-function resizeView() {
-    const CANVAS_RATIO = 9.0 / 16.0;
-    const canvas = context.app.view;
-    const {model, view} = modelView;
-
-    // 1. actual ratio
-    const emptySpace = 2 * parseInt(canvas.style.padding);
-    const actualRatio = (window.innerWidth - emptySpace) / (window.innerHeight - emptySpace);
-
-    // 2. actual size
-    let height;
-    let width;
-    if (CANVAS_RATIO <= actualRatio) {
-        height = window.innerHeight - emptySpace;
-        width = height * CANVAS_RATIO;
-    } else {
-        width = window.innerWidth - emptySpace;
-        height = width / CANVAS_RATIO;
-    }
-
-    // 3. apply
-    canvas.style.width = `${width}px`
-    canvas.style.height = `${height}px`
-    canvas.width = width * window.devicePixelRatio;
-    canvas.height = height * window.devicePixelRatio;
-
-    const canvasWidth = parseFloat(canvas.style.width) + 2 * parseFloat(canvas.style.padding);
-    const canvasHeight = parseFloat(canvas.style.height) + 2 * parseFloat(canvas.style.padding);
-    const scale = canvasWidth / (model.level.isLandscape ? model.level.width : 2 * model.level.width);
-    view.scale.set(scale * 0.77);
-    view.x = canvasWidth / 2;
-    view.y = canvasHeight / 2;
-}
