cc19ec48602d3b3e47b1cc52c0468e2e4b16d00b
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2023-07-15 15:31:50 +0300

    context

--- src/main.js
+++ src/main.js
@@ -1,21 +1,7 @@
-const context = {
-    app: null,
-};
-
-(async function bootstrap() {
-    const canvas = document.querySelector("#mainCanvas")
-    context.app = new PIXI.Application({
-        background: 'white',
-        antialias: true,
-        resolution: window.devicePixelRatio || 1,
-        view: canvas,
-        resizeTo: canvas,
-    });
-    window.onresize = resizeView;
-    await main();
-}());
+(async () => await main())()
 
 async function main() {
     await loadFont('fonts/Filmotype_Major.otf');
+    initializeContext()
     await firstLevel();
 }
