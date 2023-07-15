e78ce2a8773b88847e1c099bfb9cfd559d02efd5
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2023-07-15 15:31:56 +0300

    context

---
+++ src/context.js
@@ -0,0 +1,15 @@
+const context = {
+    app: null,
+};
+
+function initializeContext() {
+    const canvas = document.querySelector("#mainCanvas")
+    context.app = new PIXI.Application({
+        background: 'white',
+        antialias: true,
+        resolution: window.devicePixelRatio || 1,
+        view: canvas,
+        resizeTo: canvas,
+    });
+    window.onresize = resizeView;
+}
