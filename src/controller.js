d5c6d75c3c06f28215e9cd83154e1c62db92773f
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2023-07-19 14:25:25 +0300

    controller internals

--- src/controller.js
+++ src/controller.js
@@ -14,22 +14,25 @@ async function showLevel(levelId) {
 function moveFailure(event) {
     const {model} = context.modelView;
     model.mistakesCount++;
+    _showRedCrossOnMoveFailure(event);
+}
 
+function moveSuccess(slot) {
+    const {model} = context.modelView;
+    model.successSlots.push(slot);
+    _checkNextLevelOnMoveSuccess();
+}
+
+/*internals*/
+
+function _showRedCrossOnMoveFailure(event) {
     const layer = event.area.parent;
     const redCross = new RedCross(event);
     layer.addChild(redCross);
     delay(220, () => layer.removeChild(redCross));
 }
 
-function moveSuccess(slot) {
-    const {model} = context.modelView;
-    model.successSlots.push(slot);
-    _checkNextLevel();
-}
-
-/*internals*/
-
-function _checkNextLevel() {
+function _checkNextLevelOnMoveSuccess() {
     const {model} = context.modelView;
     if (model.isLevelCompleted()) {
         delay(220, () => {
