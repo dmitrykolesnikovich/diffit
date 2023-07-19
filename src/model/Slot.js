cf5f2d7b1565c868e54947e1f69a0f176f5c521a
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2023-07-19 14:44:55 +0300

    rename slot.textureId to slot.id

--- src/model/Slot.js
+++ src/model/Slot.js
@@ -1,22 +1,22 @@
 class Slot {
+    id;
     layer;
     name;
     x;
     y;
     width;
     height;
-    textureId;
 }
 
 function buildSlot(levelId, slotJson) {
     const slot = new Slot();
     const {layer, name, x, y, width, height} = slotJson
+    slot.id = `level${levelId}/${slot.name}`;
     slot.layer = layer;
     slot.name = name;
     slot.x = x;
     slot.y = y;
     slot.width = width;
     slot.height = height;
-    slot.textureId = `level${levelId}/${slot.name}`;
     return slot;
 }
