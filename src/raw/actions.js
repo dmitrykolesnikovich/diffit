0528422a39e20aaf0ad4ac9f800e42e04a8991ea
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2023-07-23 23:13:38 +0300

    _checkNextLevelOnMoveSuccess - dom

--- src/raw/actions.js
+++ src/raw/actions.js
@@ -13,6 +13,7 @@ function moveSuccess(event) {
     const {model} = engine.modelView;
     const slot = event.target;
     model.successSlots.push(slot);
+    _checkNextLevelOnMoveSuccess();
 }
 
 function moveFailure(event) {
@@ -21,9 +22,21 @@ function moveFailure(event) {
     _showRedCrossOnMoveFailure(event);
 }
 
+/*internals*/
+
 function _showRedCrossOnMoveFailure(event) {
     const layer = event.object.parentNode;
     const redCross = createFailMark(event.point);
     layer.appendChild(redCross);
     setTimeout(() => layer.removeChild(redCross), 220);
+}
+
+function _checkNextLevelOnMoveSuccess() {
+    const {model} = engine.modelView;
+    if (model.hasMaxScore()) {
+        setTimeout(() => {
+            alert(`Ура! Уровень ${model.level.id} пройден!`);
+            engine.emit('showLevel', {levelId: model.level.id % 5 + 1});
+        }, 220);
+    }
 }
